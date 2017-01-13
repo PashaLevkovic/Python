938483f8b5d20481be4565d13cc54b76b89af085
Pasha.Levkovich <pashkent96@gmail.com>
2017-01-13 15:49:53 +0300

    Добавил 2.16

---
+++ ch2/staight_line_test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from numpy import *
+c = 2
+tg = False
+L = lambda x: 4*x + 1
+
+def line(x):
+    result = []
+    for i in range(0, len(interval)):
+        l = (L(x[i])-L(c))/(x[i]-c)
+        result.append(l)
+        if result[i] == result[i-1]:
+            tg = True
+    return result, tg
+interval = []
+interval = numpy.linspace(0,10,100)
+
+result, tg = line(interval)
+print result
+if tg:
+    print "Проверка выполнена: прямая"
+
